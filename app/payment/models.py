92bc6b2808eb2c7973600ec76fe5b1cc86c245f4
Resul Quluzade <quluzade.resul13@mail.ru>
2023-11-30 17:47:40 +0400

    added Order model

--- app/payment/models.py
+++ app/payment/models.py
@@ -10,6 +10,22 @@ class Item(models.Model):
 class Order(models.Model):
     items = models.ManyToManyField(Item, related_name="items")
 
+    def make_line_items(self):
+        def callback(item):
+            return {
+                "price_data": {
+                    "currency": "rub",
+                    "unit_amount_decimal": item.price * 100,
+                    "product_data": {
+                        "name": item.name,
+                        "description": item.description,
+                    },
+                },
+                "quantity": 1,
+            }
+
+        return list(map(callback, self.items))
+
     @property
     def price(self):
         return sum(item.price for item in self.items)
