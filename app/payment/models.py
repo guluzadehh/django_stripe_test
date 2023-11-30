6ae8b7fe575b2ab6989a17658a659c157661387d
Resul Quluzade <quluzade.resul13@mail.ru>
2023-11-30 17:35:36 +0400

    added Order model

--- app/payment/models.py
+++ app/payment/models.py
@@ -5,3 +5,11 @@ class Item(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Order(models.Model):
+    items = models.ManyToManyField(Item, related_name="items")
+
+    @property
+    def price(self):
+        return sum(item.price for item in self.items)
