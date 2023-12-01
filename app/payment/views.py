9cf071490605175b45ddd453b8bc33335a6d4fc1
Resul Quluzade <quluzade.resul13@mail.ru>
2023-12-01 17:29:22 +0400

    fix view to support Order and Item buy

--- app/payment/views.py
+++ app/payment/views.py
@@ -4,31 +4,27 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, TemplateView
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Item
+from .models import Item, Order
+from .utils import make_line_item
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 class StripeSessionIdGenericAPIView(APIView):
     def get(self, request, *args, **kwargs):
-        item = get_object_or_404(Item, id=kwargs["pk"])
+        if kwargs["type"] == "order":
+            order = get_object_or_404(Order, id=kwargs["pk"])
+            line_items = order.make_line_items()
+            meta_data = {"order_id": order.id}
+        else:
+            item = get_object_or_404(Item, id=kwargs["pk"])
+            line_items = make_line_item(item)
+            meta_data = {"item_id": item.id}
 
         checkout_session = stripe.checkout.Session.create(
             payment_method_types=["card"],
-            line_items=[
-                {
-                    "price_data": {
-                        "currency": "rub",
-                        "unit_amount_decimal": item.price * 100,
-                        "product_data": {
-                            "name": item.name,
-                            "description": item.description,
-                        },
-                    },
-                    "quantity": 1,
-                },
-            ],
-            metadata={"item_id": item.id},
+            line_items=line_items,
+            metadata=meta_data,
             mode="payment",
             success_url=settings.PAYMENT_SUCCESS_URL,
             cancel_url=settings.PAYMENT_CANCEL_URL,
