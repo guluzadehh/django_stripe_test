902af075c362eac218fb08edccd3652220720f58
Resul Quluzade <quluzade.resul13@mail.ru>
2023-12-02 17:00:17 +0400

    fix payment urls

--- app/payment/urls.py
+++ app/payment/urls.py
@@ -4,8 +4,8 @@ from .views import ItemDetailView, OrderDetailView, SuccessView, CancelView
 app_name = "payment"
 
 urlpatterns = [
-    path("item/<int:pk>/", ItemDetailView.as_view(), name="item-detail"),
-    path("order/<int:pk>/", OrderDetailView.as_view(), name="order-detail"),
+    path("items/<int:pk>/", ItemDetailView.as_view(), name="item-detail"),
+    path("orders/<int:pk>/", OrderDetailView.as_view(), name="order-detail"),
     path("payment/cancel/", CancelView.as_view(), name="cancel"),
     path("payment/success/", SuccessView.as_view(), name="success"),
 ]
