deb66938bee2b91e6bea23b4689a35ce7d3f5c16
Resul Quluzade <quluzade.resul13@mail.ru>
2023-12-02 17:31:38 +0400

    fix settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = "app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["templates"],
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
